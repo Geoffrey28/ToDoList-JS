f0b371f4d2b6c8ad5e614ac861982f690e3bd1b2
Geoffrey28 <geoffreygirard28@gmail.com>
2024-07-01 15:54:59 +0200

    add notif & sort function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,12 @@
+// SELECTORS
 const tasksList = document.querySelector('.tasksList');
+const form = document.querySelector('.tasksForm');
+const taskTitle = document.querySelector('#taskname');
+const taskPrio = document.querySelector('#priority');
+const deleteBtn = document.querySelector('.deleteTasks');
+const notif = document.querySelector('.notif');
 
+// MEMORY
 const tasks = [
     {
         title: "Apprendre mon cours de JavaScript",
@@ -15,19 +22,57 @@ const tasks = [
     }
 ];
 
+// EVENTS
 window.addEventListener('load', () => {
-    for (let i = 0; i < tasks.length; i++) {
-        showTask(tasks[i]);
-    }
+    showTasks(tasks);
 });
 
-function showTask(task) {
-    let newTask = document.createElement('li');
-    let label = document.createElement('label');
-    label.classList.add('prio' + task.priority);
-    let input = document.createElement('input');
-    input.type = 'checkbox';
-    label.append(input, task.title);
-    newTask.appendChild(label);
-    tasksList.appendChild(newTask);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let task = {
+        title: taskTitle.value,
+        priority: taskPrio.value
+    }
+    tasks.push(task);
+    showTasks(tasks);
+    form.reset();
+});
+
+deleteBtn.addEventListener('click', () => {
+    let nbrDeletion = 0;
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasksList.childNodes[i].querySelector('input').checked) {
+            console.log(tasks[i]);
+            tasks.splice(i, 1);
+            console.log(tasks[i]);
+            nbrDeletion++;
+        }
+    }
+    showNotification(nbrDeletion);
+    showTasks(tasks);
+});
+
+
+// FUNCTIONS
+function showTasks(tasks) {
+    sortTasks();
+    tasksList.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+        let newTask = document.createElement('li');
+        let label = document.createElement('label');
+        label.classList.add('prio' + tasks[i].priority);
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        label.append(input, tasks[i].title);
+        newTask.appendChild(label);
+        tasksList.appendChild(newTask);
+    }
+}
+
+function sortTasks() {
+    tasks.sort((a, b) => a.priority - b.priority);
+}
+
+function showNotification(nbrDeletion) {
+    notif.textContent = nbrDeletion + ' tâches supprimées avec succès';
 }
